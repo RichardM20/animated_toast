2836ba395e416b5c2fad79f19a608e15c502d5ea
RichardM20 <richardoficial2020@gmail.com>
2025-03-23 14:32:16 -0300

    feat: actions

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -11,7 +11,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(title: Text('Animated Toast Examples')),
         body: ToastExampleScreen(),
